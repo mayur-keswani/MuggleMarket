d12c8565018ab1aed21c15b58fdff10c970d8449
mayur-keswani <mayurkeswani2001@gmail.com>
2023-07-17 08:50:55 +0530

    fix: cart ui

--- src/component/order-summary/Cart.jsx
+++ src/component/order-summary/Cart.jsx
@@ -15,56 +15,58 @@ const Cart = () => {
   } = useContext(UserContext);
   const navigate = useNavigate();
 
-  async function proceedToCheckout (){
+  async function proceedToCheckout(cartItems) {
     try {
-      const { data } = await checkoutAPI(orderItems);
+      const { data } = await checkoutAPI(cartItems);
       navigate("/checkout");
     } catch (error) {}
-  };
+  }
 
-  function getTotalPrice(){
+  function getTotalPrice() {
     let totalPrice = 0;
     cart?.items.forEach((cartItem) => {
       totalPrice = cartItem?.quantity * cartItem?.item?.price;
     });
-    return totalPrice
+    return totalPrice;
   }
-  
-  return showCartItems ? (
+
+  return (
     <div className="fixed bottom-0 left-0 bg-primary w-full font-bold overflow-scroll max-h-[calc(100vh-6rem)]">
-      <div className="flex items-center justify-between flex-col h-full mx-4 py-2">
+      <div
+        className={`flex items-center justify-between ${showCartItems &&
+          "flex-col"} h-full mx-4 py-2`}
+      >
         <button
           className="btn btn-primary"
-          onClick={() => setShowCartItems(false)}
+          onClick={() => setShowCartItems((prevState) => !prevState)}
         >
-          <BsFillArrowDownCircleFill size={'2rem'}/>
-        </button>
-        <OrderSummary />
-        <button
-          className="btn btn-info px-5 py-2 mx-auto"
-          onClick={proceedToCheckout}
-        >
-          Continue
-        </button>
-      </div>
-    </div>
-  ) : (
-    <div className="fixed bottom-0 left-0 bg-primary h-24 w-full">
-      <div className="flex items-center justify-between h-full font-bold mx-4 ">
-        <div className="flex flex-row">
-          <button onClick={() => setShowCartItems(true)}>
+          {showCartItems ? (
+            <BsFillArrowDownCircleFill size={"2rem"} />
+          ) : (
             <BsFillArrowUpCircleFill />
-          </button>
-          <span className="mx-2"> Your Order ({cart?.items.length})</span>
-        </div>
-
-        <div>
-          Subtotal :<span className="text-red"> {getTotalPrice()}</span>
-        </div>
-
-        <button className="btn btn-info px-5 py-2" onClick={proceedToCheckout}>
-          Continue
+          )}
         </button>
+        {showCartItems ? (
+          <OrderSummary />
+        ) : (
+          <div className="flex flex-row justify-between">
+            <p className="mx-2">
+              {" "}
+              Your Order <span className="text-muted">({cart?.items.length})</span>
+            </p>
+            <div>
+              Subtotal :<span className="text-red"> {getTotalPrice()}</span>
+            </div>
+          </div>
+        )}
+        <div className="text-right">
+          <button
+            className="btn btn-info px-5 py-2 mx-auto"
+            onClick={() => proceedToCheckout(cart.items)}
+          >
+            Continue
+          </button>
+        </div>
       </div>
     </div>
   );
